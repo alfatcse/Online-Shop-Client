970c6105188a50343094750c11268a78be2240c3
Alfat <alfat.cse.21@gmail.com>
2022-12-27 18:57:43 +0100

    chatting show message

--- src/Pages/Shared/Chat/Chat.js
+++ src/Pages/Shared/Chat/Chat.js
@@ -5,8 +5,9 @@ import { useLoaderData } from "react-router-dom";
 const socket = io.connect("http://localhost:5009/");
 const Chat = () => {
   const bookingId = useLoaderData();
-  const [message, setMessage] = useState("");
-  const [getMessage, setGetMessage] = useState("");
+  const [message, setMessage] = useState([]);
+  const [getMessage, setGetMessage] = useState([]);
+  const [singleMes, setSingleMes] = useState([]);
   socket.emit("joinRoom", bookingId);
   const handleSend = () => {
     socket.emit("reactEvent", { message, room: bookingId });
@@ -15,31 +16,44 @@ const Chat = () => {
   useEffect(() => {
     socket.on("showMessage", (data) => {
       console.log(data);
-      setGetMessage(data.message);
+      setGetMessage([data.message]);
     });
   }, [socket]);
+  console.log("mes", getMessage);
   return (
     <body class="flex flex-col items-center justify-center min-h-screen bg-blue-300 text-gray-800 p-10">
       <div class="flex flex-col flex-grow w-full max-w-xl bg-blue-100 shadow-xl rounded-lg overflow-hidden">
         <div class="flex flex-col flex-grow h-0 p-4 overflow-auto">
-          <div class="flex w-full mt-2 space-x-3 max-w-xs">
-            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-            <div>
-              <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-                <p class="text-sm">{getMessage}</p>
+          {message.map((m) => (
+            <p className=" text-red-800">Sender: {m}</p>
+          ))}
+          {getMessage.map((g) => (
+            <p className="text-blue-800">Receiver: {g}</p>
+          ))}
+          {getMessage[0].map((m) => (
+            <div class="flex w-full mt-2 space-x-3 max-w-xs">
+              <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+              <div>
+                <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                  <p class="text-sm">{m}</p>
+                </div>
+                <span class="text-xs text-gray-500 leading-none">
+                  2 min ago
+                </span>
               </div>
-              <span class="text-xs text-gray-500 leading-none">2 min ago</span>
             </div>
-          </div>
+          ))}
+           {message.map((g) => (
           <div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
             <div>
               <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                <p class="text-sm">{message}</p>
+                <p class="text-sm">{g}</p>
               </div>
               <span class="text-xs text-gray-500 leading-none">2 min ago</span>
             </div>
+            
             <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-          </div>
+          </div>    ))}
         </div>
 
         <div class="bg-gray-300 p-4 flex ">
@@ -48,7 +62,7 @@ const Chat = () => {
             type="text"
             placeholder="Type your message…"
             id="chat-input"
-            onBlur={(e) => setMessage(e.target.value)}
+            onBlur={(e) => setMessage([...message, e.target.value])}
           />
           <button onClick={handleSend} className="mx-2 btn btn-outline">
             <FiSend></FiSend>
